6b5ce19daf76b83bdf62da14f2eee503713846e8
TahidFlexion <163385747+TahidFlexion@users.noreply.github.com>
2024-11-29 15:22:39 +0530

    setup for deploy

---
+++ .next/static/chunks/pages/authpage-c6734be2dacd0bae.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[287],{7015:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/authpage",function(){return u(7856)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(7015)),_N_E=_.O()}]);
